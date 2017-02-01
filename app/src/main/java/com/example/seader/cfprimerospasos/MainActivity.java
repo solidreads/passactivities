c555dd391a96997597c3f2bb661740f69bfff383
dafma <mar_tp@hotmail.es>
2017-02-01 14:10:12 -0600

    añadido segundo activity

--- app/src/main/java/com/example/seader/cfprimerospasos/MainActivity.java
+++ app/src/main/java/com/example/seader/cfprimerospasos/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.seader.cfprimerospasos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void lanzarMensajeCorto(){
+        Toast.makeText(this, "Soy un toast corto", Toast.LENGTH_SHORT).show();
+    }
+
+    public void lanzarMensajeLargo(){
+        Toast.makeText(this, "Soy un mensaje largo", Toast.LENGTH_LONG).show();
+    }
+
+    public void llamarSegundoActivity(View view){
+        lanzarMensajeCorto();
+        Intent intent = new Intent(this, SegundoActivity.class);
+        //va a ser que arranque nuestra seunda pantallla
+        startActivity(intent);
+
     }
 }
